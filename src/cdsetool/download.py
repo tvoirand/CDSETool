a31f4c943e2feee803380fcd404aeda4644f1d5c
Thibaut Voirand <thibautvoirand@gmail.com>
2024-12-18 18:28:06 +0100

    Add first version of node downloading function using metadata

--- src/cdsetool/download.py
+++ src/cdsetool/download.py
@@ -6,15 +6,14 @@ all features in a result set.
 """
 
 import fnmatch
-import json
 import os
 import random
 import shutil
 import tempfile
 import time
 from typing import Any, Dict, Generator, List, Union
+from xml.etree import ElementTree as etree
 
-import requests
 from requests import Session
 from requests.exceptions import ChunkedEncodingError
 from urllib3.exceptions import ProtocolError
@@ -30,66 +29,102 @@ from cdsetool.monitor import NoopMonitor, StatusMonitor
 from cdsetool.query import FeatureQuery
 
 
+def _xml_to_dataobj_info(element: etree.Element) -> Dict[str, Any]:
+    assert etree.iselement(element)
+    assert element.tag == "dataObject"
+    data = {}
+    data["id"] = element.attrib["ID"]
+    elem = element.find("byteStream")
+    data["size"] = int(elem.attrib["size"])
+    elem = element.find("byteStream/fileLocation")
+    data["href"] = elem.attrib["href"]
+    elem = element.find("byteStream/checksum")
+    data["checksum_algorithm"] = elem.attrib["checksumName"].lower()
+    data["checksum_value"] = elem.text
+    return data
+
+
+def _href_to_url(odata_url: str, product_id: str, product_name: str, href: str) -> str:
+    path = "/".join([f"Nodes({item})" for item in href.split("/")])
+    return f"{odata_url}/Products({product_id})/Nodes({product_name})/{path}/$value"
+
+
 def search_nodes(
-    node_url: str, pattern: str, exclude: bool = False
+    manifest_file_path: str, pattern: str, exclude: bool = False
 ) -> List[Dict[str, Any]]:
     """
-    Search for a given pattern in product node tree obtained from a CDSE OData API url.
+    Search for product tree nodes matching a given pattern within a manifest file.
 
     If "exclude" is set to False, only nodes that match pattern are returned. If it's
     set to True, only nodes that do not match pattern are returned.
     """
-    input_nodes = json.loads(requests.get(node_url, timeout=20).text)["result"]
-    output_nodes = []
-    for node in input_nodes:
-        # If this node is a dir, call 'search_nodes' recursively
-        if node["ContentLength"] == 0:
-            output_nodes.extend(search_nodes(node["Nodes"]["uri"], pattern))
+    nodes = []
+    xmldoc = etree.parse(manifest_file_path)
+    data_obj_section_elem = xmldoc.find("dataObjectSection")
+    for elem in data_obj_section_elem.iterfind("dataObject"):
+        dataobj_info = _xml_to_dataobj_info(elem)
+        match = fnmatch.fnmatch(dataobj_info["href"].lower(), pattern)
+        if match and not exclude or exclude and not match:
+            nodes.append(dataobj_info)
+    return nodes
 
-        # If it's a file, check for pattern match and optionally append to results
-        elif node["ContentLength"] >= 0:
-            match = fnmatch.fnmatch(node["Name"], pattern)
-            if match and not exclude or exclude and not match:
-                output_nodes.append(node)
 
-    return output_nodes
+def download_file(url: str, path: str, session: Session):
+    """
+    Download one specific file.
+    """
+    response = session.get(url, stream=True)
+    if response.status_code == 200:
+        with open(path, "wb") as outfile:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    outfile.write(chunk)
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+        print(response.text)
 
 
 def download_node(
-    feature_id,
+    feature,
     path: str,
-    username: str,
-    password: str,
     nodefilter_pattern: str,
+    options: Union[Dict[str, Any], None] = None,
 ):
     """
     Download specific files within a feature using node filtering.
     """
-    url = (
-        "https://download.dataspace.copernicus.eu/"
-        f"odata/v1/Products({feature_id})/Nodes"
+    options = options or {}
+    session = _get_credentials(options).get_session()
+
+    # Get product URL and name and create output dir
+    odata_url = "https://download.dataspace.copernicus.eu/odata/v1"
+    product_name = feature["properties"]["title"]
+    output_product_path = os.path.join(path, product_name)
+    os.makedirs(output_product_path, exist_ok=True)
+
+    # Download manifest file using hardcoded paths to find URL
+    manifest_file_path = os.path.join(output_product_path, "manifest.safe")
+    download_file(
+        _href_to_url(odata_url, feature["id"], product_name, "manifest.safe"),
+        manifest_file_path,
+        session,
     )
-    nodes = search_nodes(url, nodefilter_pattern)
+
+    # List nodes that match pattern based on manifest file contents
+    nodes = search_nodes(manifest_file_path, nodefilter_pattern)
 
     for node in nodes:
-        # Authenticate to CDSE
-        session = Credentials(username, password).get_session()
+        output_file_path = os.path.join(
+            output_product_path,
+            node["href"][2:],  # TODO: Check if this needs to be generalized
+        )
+        os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
 
-        url = f"{node['Nodes']['uri'][:-5]}$value"
-        response = session.get(url, stream=True)
-
-        # Download file if request was successful
-        if response.status_code == 200:
-            with open(
-                os.path.join(path, node["Name"]),
-                "wb",  # TODO: Reproduce directories tree structure
-            ) as file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:
-                        file.write(chunk)
-        else:
-            print(f"Failed to download file. Status code: {response.status_code}")
-            print(response.text)
+        download_file(
+            _href_to_url(odata_url, feature["id"], product_name, node["href"][2:]),
+            output_file_path,
+            session,
+        )
 
 
 def download_feature(
